5a29166d791f702a482a4738d5b492280ecde208
Josh Kearl <Josh.kearl@gmail.com>
2018-08-04 17:59:47 -0600

    added http service and injected to product-description

---
+++ src/app/product.service.ts
@@ -0,0 +1,19 @@
+import { Injectable } from '@angular/core';
+import {Http, Response} from '@angular/http';
+import { map } from 'rxjs/operators';
+import { Observable } from 'rxjs/Observable';
+
+@Injectable()
+export class ProductService {
+
+  private _albumUrl: string = '../assets/album.json'
+
+  constructor(private _http: Http) { }
+
+  getAlbum(id: number){
+   return this._http.get(this._albumUrl).pipe(map((response) => {
+      response.json()
+    }))
+  }
+
+}
